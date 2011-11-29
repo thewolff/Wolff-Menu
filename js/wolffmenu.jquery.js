e1ec55ac83521e323ca28515f2ae5a13622c763d
Joseph Wolff <thewolff@jdwolff.com>
2011-11-29 06:09:51 -0500

    Adding the initial JS files + jQuery

---
+++ js/wolffmenu.jquery.js
@@ -0,0 +1,12 @@
+// IIFE to ensure safe use of $
+(function($){
+
+	// Create plugin
+	$.fn.wolffMenu = function(){
+	
+		// Ensure chaining works
+		return this.each(function(){
+		
+		});
+	}
+})(jQuery);
