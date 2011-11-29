109b0bab4ed4e0eeef02e639bec7e0a33c7f7ca6
Joseph Wolff <thewolff@jdwolff.com>
2011-11-29 07:17:08 -0500

    Changed file names, added license.

---
+++ js/jquery.wolffmenu.js
@@ -0,0 +1,18 @@
+/**
+ * jQuery.WolffMenu
+ * Copyright (c) 2011 Joseph Wolff - thewolff(at)jdwolff(dot)com | http://jdwolff.com
+ * Dual licensed under MIT and GPL.
+ */
+
+// IIFE to ensure safe use of $
+(function($){
+
+	// Create plugin
+	$.fn.wolffMenu = function(){
+	
+		// Ensure chaining works
+		return this.each(function(){
+		
+		});
+	}
+})(jQuery);
